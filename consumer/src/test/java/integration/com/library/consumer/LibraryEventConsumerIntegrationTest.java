68319a148330b6628b487e7962d5d8280f210618
Mohamed Elhamra <mohamed.elhamra@capgemini.com>
2024-08-14 16:12:49 +0200

    Integration test for posting new LibraryEvent

---
+++ consumer/src/test/java/integration/com/library/consumer/LibraryEventConsumerIntegrationTest.java
@@ -0,0 +1,101 @@
+package com.library.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.library.consumer.consumer.LibraryEventConsumer;
+import com.library.consumer.entity.LibraryEvent;
+import com.library.consumer.repository.LibraryEventRepository;
+import com.library.consumer.service.LibraryEventService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.ArgumentMatchers.isNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-topic"}, partitions = 3)
+// override application.yml config
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}"})
+public class LibraryEventConsumerIntegrationTest {
+
+    @Autowired
+    EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    KafkaTemplate<Integer, String> kafkaTemplate;
+
+    // Holds all the listener container (MessageListenerContainer)
+    @Autowired
+    KafkaListenerEndpointRegistry endpointRegistry;
+
+    @SpyBean
+    LibraryEventConsumer libraryEventConsumer;
+
+    @SpyBean
+    LibraryEventService libraryEventService;
+
+    @Autowired
+    LibraryEventRepository libraryEventRepository;
+
+    @Value("${spring.kafka.consumer.groupId}")
+    String groupId;
+
+    @BeforeEach
+    void setUp() {
+        var container = endpointRegistry.getListenerContainers()
+                .stream().filter(messageListenerContainer ->
+                        Objects.equals(messageListenerContainer.getGroupId(), groupId))
+                .toList().get(0);
+        // wait until all partitions are assigned into the container
+        ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic());
+    }
+
+    @AfterEach
+    void tearDown() {
+        libraryEventRepository.deleteAll();
+    }
+
+    @Test
+    void publishNewLibraryEvent() throws InterruptedException, JsonProcessingException {
+        // GIVEN
+        String json = " {\"libraryEventId\":null,\"libraryEventType\":\"NEW\",\"book\":{\"bookId\":456,\"bookName\":\"Kafka Using Spring Boot\",\"bookAuthor\":\"Mohamed\"}}";
+
+        // WHEN
+        kafkaTemplate.sendDefault(json);
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await(3, TimeUnit.SECONDS);
+
+        // THEN
+        verify(libraryEventConsumer, times(1)).onMessage(isA(ConsumerRecord.class));
+        verify(libraryEventService, times(1)).processLibraryEvent(isA(ConsumerRecord.class));
+
+        List<LibraryEvent> libraryEventList = (List<LibraryEvent>) libraryEventRepository.findAll();
+        assert libraryEventList.size() == 1;
+        libraryEventList.forEach(libraryEvent -> {
+            assert libraryEvent.getLibraryEventId() != null;
+            assertEquals(456, libraryEvent.getBook().getBookId());
+        });
+    }
+
+}
